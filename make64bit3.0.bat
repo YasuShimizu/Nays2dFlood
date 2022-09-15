40707c256a97bd2c283e531d0845df2d76a69a64
starfieldsKt <keita.hoshino.1001@gmail.com>
2022-09-15 17:39:06 +0900

    ファイルの場所を修正

--- make64bit3.0.bat
+++ make64bit3.0.bat
@@ -1,15 +1,15 @@
-ifort 	nays2d_flood\common_hh.f90 ^
-	nays2d_flood\GridCoordMod.F90 ^
-	nays2d_flood\GridCondMod.F90 ^
-	nays2d_flood\interpolation.F90 ^
-	nays2d_flood\transform.F90 ^
-	nays2d_flood\WriteCGNS.F90 ^
-	nays2d_flood\output_hotstart.f90 ^
-	nays2d_flood\read_hotstart_conditions.f90 ^
-	nays2d_flood\flow_calculation.f90 ^
-	nays2d_flood\libs\cgnsdll.lib ^
-	nays2d_flood\libs\iriclib.lib ^
-	nays2d_flood\nays2d_flood.f90 /MD /Qopenmp /nostandard-realloc-lhs -o nays2dflood_v5_64bit\nays2d_flood.exe
+ifort 	.\src\common_hh.f90 ^
+		.\src\GridCoordMod.F90 ^
+		.\src\GridCondMod.F90 ^
+		.\src\interpolation.F90 ^
+		.\src\transform.F90 ^
+		.\src\WriteCGNS.F90 ^
+		.\src\output_hotstart.f90 ^
+		.\src\read_hotstart_conditions.f90 ^
+		.\src\flow_calculation.f90 ^
+		.\src\libs\cgnsdll.lib ^
+		.\src\libs\iriclib.lib ^
+		.\src\nays2d_flood.f90 /MD /Qopenmp /nostandard-realloc-lhs -o .\install\nays2d_flood.exe
 
 del *.obj 
 del *.mod 
